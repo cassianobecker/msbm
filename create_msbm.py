Fixed bug in CH-divergence computation

create_msbm.py
@@ -2,71 +2,103 @@ import numpy as np
 import numpy.random as npr
 import sys
 import pickle
+import time
+import pdb
 from random import randint
 import scipy as sp
-def SNR(gamma,pi):
-    """
-    Criterion for detectability on the general SBM for community weights gamma
-    and connectivity matrix pi
-    Parameters
-    ----------
-    gamma : double array 
-        probability vector representing community weights
-    pi : double array
-        vector of probabilities representing inter connection probabilities
-    """
-    #We construct the matrix whose columns are the community profiles
-    PQ = np.matmul(np.diag(gamma),pi)
-    snr = 500
-    #Iterate over all different pairs of columns
-    for i in range(0,PQ.shape[2]-1):
-    	for j in range(0,PQ.shape[2]-1):
-    		if i != j:
-    			snr = min(snr, CHDiv(PQ[:,i],PQ[:,j]))
-    return snr
+from scipy.stats import beta
+from scipy import optimize
+
+def getavgDeg(gamma, pi,n):
+	"""
+	Criterion for detectability on the general SBM for community weights gamma
+	and connectivity matrix pi
+	Parameters
+	----------
+	gamma : double array 
+		probability vector representing community weights
+	pi : double array
+		vector of probabilities representing inter connection probabilities
+	"""
+	#We construct the matrix whose columns are the community profiles
+	PQ = np.matmul(np.diag(gamma[0]),pi)
+	comDegs = np.sum(PQ*np.log(n),axis=0)
+	#Now take the weighted average
+	avgDeg =  np.sum(gamma[0]*comDegs)
+
+	return avgDeg
+
+def getSNR(gamma, pi):
+	"""
+	Criterion for detectability on the general SBM for community weights gamma
+	and connectivity matrix pi
+	Parameters
+	----------
+	gamma : double array 
+		probability vector representing community weights
+	pi : double array
+		vector of probabilities representing inter connection probabilities
+	"""
+	#We construct the matrix whose columns are the community profiles
+	PQ = np.matmul(np.diag(gamma[0]),pi)
+	snr = 500
+	com_i = 0
+	com_j = 0
+	#Iterate over all different pairs of columns
+	for i in range(0,PQ.shape[1]-1):
+		for j in range(0,PQ.shape[1]-1):
+			if i != j:
+				prof_i = PQ[:,i]
+				prof_j = PQ[:,j]
+				new_snr = CHDiv(prof_i,prof_j)
+				if new_snr <= snr:
+					snr = new_snr
+					com_i = i
+					com_j = j
+	return snr, com_i, com_j 
 
 def CHDiv(theta_i,theta_j):
-    """
-    Chernoff-Hellinger divergence of two community profiles
-    detectability depends on whether the CHDiv is greater than 1.
-    This is an f-divergence and a generalization of relative-entropy.
-    Parameters
-    ----------
-    theta_i : double array 
-        profile for community i
-    theta_j : double array
-        profile for community j
-    """
-    #Define the function f
-    def fun(t):
-    	return -np.sum(t*theta_i + (1-t)*theta_j - (theta_i**t)*(theta_j**(1-t)))
+	"""
+	Chernoff-Hellinger divergence of two community profiles
+	detectability depends on whether the CHDiv is greater than 1.
+	This is an f-divergence and a generalization of relative-entropy.
+	Parameters
+	----------
+	theta_i : double array 
+		profile for community i
+	theta_j : double array
+		profile for community j
+	"""
+	#Define the function f
+	def fun(t):
+		return -np.sum(t*theta_i + (1-t)*theta_j - (theta_i**t)*(theta_j**(1-t)))
 
-    #Minimize over t
-    res = np.optimize.minimize(fun,[0.5],bounds= ((0,1)))
-    chdiv = -fun(res.x)
-    return chdiv
+	#Minimize over t
+	res = optimize.minimize(fun,(0.5),bounds= ((0,1),))
+	chdiv = -fun(res.x)
+	return chdiv
 
 def accept_prob(e,eprime,T):
-    """
-    accept_prob acceptance probability for every step of
-    the simulated annealing. We accept the new state whenever
-    it is a lower energy state, otherwise we accept with a probability
-    that decays exponentially in proportion to the difference of energies
-    and is affected by a temperature parameter. 
-    Parameters
-    ----------
-    e : double 
-        energy of current state
-    eprime : double
-        energy of proposed state
-    T : double
-    	current temperature parameter
-    """
-    if eprime < e:
-        p = 1
-    else:
-        p = exp(-(eprime - e)/T)
-    return p
+	"""
+	accept_prob acceptance probability for every step of
+	the simulated annealing. We accept the new state whenever
+	it is a lower energy state, otherwise we accept with a probability
+	that decays exponentially in proportion to the difference of energies
+	and is affected by a temperature parameter. 
+	Parameters
+	----------
+	e : double 
+		energy of current state
+	eprime : double
+		energy of proposed state
+	T : double
+		current temperature parameter
+	"""
+	if eprime < e:
+		p = 1
+	else:
+		p = np.exp(-(eprime - e)/T)
+	return p
 
 
 # ############# SAMPLING FUNCTIONS #######################
@@ -77,143 +109,188 @@ def accept_prob(e,eprime,T):
 
 
 def get_gamma_pi(
-	vdir, mii, vii, mij, vij, 
+	dii, dij,vdir = 0.015, 
 	Q = 4, sampling = 'unconstrained', SNR=None,
-	maxIter = 100, tol = 1e-07
-):
-    """
-    get_gamma_pi a function that samples gamma (the community
-    weights or community importance) from a homogeneous Dirichlet
+	maxIter = 100, tol = 1e-07, n = 100):
+	"""
+	get_gamma_pi a function that samples gamma (the community
+	weights or community importance) from a homogeneous Dirichlet
 	distribution (which has mean, a uniform distribution)
 	and pi (the connection probabilities), by specifying
 	means and variances. Optionally, we can constraint the sample to 
 	have a target Dt f-divergence (E. Abbe and C. Sandon. 2015)
 	which will determine asymptotic detectability. For the constrained
 	sampling we use simulated annealing.
-    Parameters
-    ----------
-    Q : int 
-        the number of communities. Defaults to 4
-    sampling : str
-        the type of sampling to be made. The options are
-        'deterministic', 'unconstrained' and 'constrained'
-        which correspond to deterministic annealing 
-    vdir : double
-    	variance of the homogeneous dirichlet distribution
-    mii : double
-    	mean of the whithin community beta distributions
-    vii : double
-    	variance of the whithin community beta distributions
-    mij : double
-    	mean of the across community beta distributions
-    vii : double
-    	variance of the across community beta distributions
-    SNR : double
-    	target signal to noise ratio for constrained sampling
-    maxIter : int 
-    	maximum number of iterations for the simulated annealing
-    """
-    #Obtain the alphas with target var and assuming a uniform mean
-    if sampling not in {'constrained','unconstrained'}:
-    	sys.exit("Unsupported sampling type")
+	Parameters
+	----------
+	Q : int 
+		the number of communities. Defaults to 4
+	sampling : str
+		the type of sampling to be made. The options are
+		'deterministic', 'unconstrained' and 'constrained'
+		which correspond to deterministic annealing 
+	vdir : double
+		variance of the homogeneous dirichlet distribution
+	dii : double
+		average degree whithin community, it's assumed to grow logarithmically.
+	dij : double
+		average degree across community, it's assumed to grow logarithmically.
+	SNR : double
+		target signal to noise ratio for constrained sampling
+	maxIter : int 
+		maximum number of iterations for the simulated annealing
+	n : int
+		the number of nodes. Defaults to 100. It's used to produce a logarithmic degree
+		interconnection matrix pi. 
+	"""
+	#Obtain the alphas with target var and assuming a uniform mean
+	if sampling not in {'constrained','unconstrained'}:
+		sys.exit("Unsupported sampling type")
 
-    alpha = ((Q-1)/(vdir*Q^2)+1)/Q
-    gamma = npr.dirichlet(np.repeat(alpha, Q),1)
-    
-    #Beta parameters alpha_ii and beta_ii
-    alpha_ii = (mii^2*(1-mii) - mii*vii)/vii
-    beta_ii  = (1/vii)*(mii*(1-mii) - vii)*(1-mii)
-    #Beta parameters alpha_ij and beta_ij
-    alpha_ij = (mij^2*(1-mij) - mij*vij)/vij
-    beta_ij  = (1/vij)*(mij*(1-mij) - vij)*(1-mij)
+	alpha = ((Q-1)/(vdir*Q**2)+1)/Q
+	gamma = npr.dirichlet(np.repeat(alpha, Q),1)
+	
+	#Beta parameters alpha_ii and beta_ii
+	mii = np.double(dii)/n
+	vii = (np.double(dii)/20)/(n)
 
-    pi = np.zeros((Q,Q))
-    for i in range(Q):
-    	for j in range(Q):
-    		if i == j:
-    			pi[i,j] = npr.beta(alpha_ii,beta_ii)/2
+	alpha_ii = ((mii**2)*(1-mii) - mii*vii)/vii
+	beta_ii  = (1/vii)*(mii*(1-mii) - vii)*(1-mii)
+	#Beta parameters alpha_ij and beta_ij
+	mij = np.double(dij)/n
+	vij = (np.double(dij)/20)/(n)
+	alpha_ij = ((mij**2)*(1-mij) - mij*vij)/vij
+	beta_ij  = (1/vij)*(mij*(1-mij) - vij)*(1-mij)
 
-    		if i < j:
-    			pi[i,j] = npr.beta(alpha_ij,beta_ij)
+	pi = np.zeros((Q,Q))
+	for i in range(Q):
+		for j in range(Q):
+			if i == j:
+				pi[i,j] = npr.beta(alpha_ii,beta_ii)/2
 
-    pi = pi + np.transpose(pi)
+			if i < j:
+				pi[i,j] = npr.beta(alpha_ij,beta_ij)
 
-    if sampling == 'unconstrained':
-    	return gamma, pi
+	pi = pi + np.transpose(pi)
+	if sampling == 'unconstrained':
+		return gamma, pi
 
-    print('Beggining constrained sampling with:{:d} iterations'.format(maxIter))
-    print('---------------------------------------------------')
-    T_init= 100
-    if SNR is None:
-    	sys.exit("Unspecified SNR. The detectability threshold is SNR = 1")
-    counter = 0
-    cSNR = SNR(gamma, pi)
-    e = (SNR - cSNR)^2
-    while counter <= maxIter and e > tol:
-		print('Iteration:{:d}, Target SNR: {:03f}, Current SNR: {:03f}, Energy: {:06f}'.format(counter,SNR,cSNR,e))
-		#We do the local step and compute the energy of that state
+	print('Beggining constrained sampling with:{:d} iterations'.format(maxIter))
+	print('---------------------------------------------------')
+	T_init= 1.01
+	#Normalize the average degree of the network to its expected value
+	targetDeg = (np.double(dii) + (Q-1)*np.double(dij))/Q
+	avgDeg = getavgDeg(gamma,pi * (n/np.log(n)),n)
+	pi = pi * targetDeg/avgDeg
+	if SNR is None:
+		sys.exit("Unspecified SNR. The detectability threshold is SNR = 1")
+	counter = 0
+	pi_constant = pi * (n/np.log(n))
+	cSNR, com_i, com_j  = getSNR(gamma,pi_constant)
+
+	e = (SNR - cSNR)**2
+	while counter <= maxIter and e > tol:
+		avgDeg =  getavgDeg(gamma,pi * (n/np.log(n)),n)
+		if counter % 1 == 0:
+			print('Iteration:{:d}, Target SNR: {:03f}, Current SNR: {:03f}, Energy: {:08f}, avgDeg: {:03f}'.format(counter,SNR,cSNR,e,avgDeg))
 		counter += 1
-		if npr.binomial(1, 0.7, 1) == 1:
-			pi_prime = pi
-			#Select an entry at random and resample from the prior beta
-			ind_i = randint(0,Q-1)
+		pi_prime = pi
+		gamma_prime = gamma
+		#We do the local step and compute the energy of that state
+		#With probability .5 we change gamma with probability .5 we change pi
+		#We do a random step where the average degree of the network is preserved
+		if npr.binomial(1, .5, 1) == 1:
+			xx = npr.binomial(1, .5, 1)[0]
+			ind_i = xx*com_i + (1-xx)*com_j
 			ind_j = randint(0,Q-1)
-			new_value = npr.beta(alpha_ii,beta_ii)
-			pi_prime[ind_i,ind_j] = new_value
-			pi_prime[ind_j,ind_i] = new_value
-			gamma_prime = gamma
-		else: 
-			gamma_prime = npr.dirichlet(np.repeat(alpha, Q),1)
-			gamma_prime = 0.95*gamma_prime + 0.05*gamma
-			pi_prime = pi
+			#We take a weighted average of a new beta sample and the current one, randomly sampling from 
+			if ind_i == ind_j:
+				new_value = npr.beta(alpha_ii,beta_ii)
+			else:
+				new_value = npr.beta(alpha_ij,beta_ij)
+			pi_prime[ind_i,ind_j] = 0.99*pi_prime[ind_i,ind_j] + 0.01*new_value
+			pi_prime[ind_j,ind_i] = 0.99*pi_prime[ind_j,ind_i] + 0.01*new_value
+		#And for gamma
+		else:
+			gamma_prime = 0.99*gamma_prime + 0.01*npr.dirichlet(np.repeat(alpha, Q),1)
+		#We renormalize to preserve the average degree
+		avgDeg_prime = getavgDeg(gamma_prime,pi_prime * (n/np.log(n)),n)
+		pi_prime = pi_prime * (targetDeg/avgDeg_prime)
 
-		cSNR_prime = SNR(gamma_prime, pi_prime)
-		eprime = (SNR - cSNR_prime)
-		#We obtain the acceptance probability   
-		p = accept_prob(e,eprime,T_init/counter^2)
-		print('Acceptance probaility: {:04f}'.format(p))
+		#Chernoff-Hellinger Divergence must be computed on a constant matrix pi, before the n/log(n) factor
+		pi_prime_constant = pi_prime * (n/np.log(n))
+		cSNR_prime, com_i_prime, com_j_prime = getSNR(gamma_prime, pi_prime_constant)
+
+		eprime = (SNR - cSNR_prime)**2
+		if(counter > 1500):
+			pdb.set_trace()
+			print("Current min divergence communities:")
+			print("Communities: {:d} and {:d}".format(com_i,com_j))
+			print("Active Community Profiles")
+			PQ = np.matmul(np.diag(gamma[0]),pi_constant)
+			prof_i = PQ[:,com_i]
+			prof_j = PQ[:,com_j]
+			print(np.array[prof_i,prof_j])
+			print("proposed SNR")
+			print(cSNR_prime)
+			print("Current pi_constant")
+			print(pi_constant * np.log(n))
+			print("Proposed pi_constant")
+			print(pi_prime_constant * np.log(n))
+			print("Difference:")
+			print((pi_prime_constant-pi_constant)*np.log(n))
+			print("----------------")
+		#We obtain the acceptance probability
+		p = accept_prob(e,eprime,T_init/(counter))
+		if(counter > 1500):
+			pdb.set_trace()
 		if npr.binomial(1, p, 1) == 1:
+			if(eprime > e): print("Jumping to state with higher Energy")
 			e = eprime
 			gamma = gamma_prime
 			pi = pi_prime
+			pi_constant = pi * (n/np.log(n))
 			cSNR = cSNR_prime
-	print('Finished simulated annealing after:{:d} iterations '.format(counter))
-	print('Resulting SNR is:{:03f}, target was {:03f} '.format(cSNR,SNR))
+			com_i = com_i_prime
+			com_j = com_j_prime
+		print("-----------------")
+	print('Finished simulated annealing after:{:d} iterations '.format(counter-1))
+	print('Resulting SNR is:{:04f}, target was {:04f} '.format(cSNR,SNR))
 	return gamma, pi
 
 
 def get_rho():
 
-    rho = np.array([0.2, 0.5, 0.7, 0.3])
+	rho = np.array([0.2, 0.5, 0.7, 0.3])
 
-    return rho/np.sum(rho)
+	return rho/np.sum(rho)
 
 
 def sample_Y(rho, K):
 
-    Y = npr.multinomial(1, rho, K)
+	Y = npr.multinomial(1, rho, K)
 
-    return Y.astype(float)
+	return Y.astype(float)
 
 
 def sample_Z(gamma, N):
 
-    Z = npr.multinomial(1, gamma, N)
+	Z = npr.multinomial(1, gamma, N)
 
-    return Z.astype(float)
+	return Z.astype(float)
 
 def sample_X_und(Pi, Z):
 
-    N = Z.shape[0]
-    X = np.zeros((N, N))
-    for i in range(N):
-            zi = find_row(Z[i, :])
-            for j in range(i, N):
-                zj = find_row(Z[j, :])
-                X[i, j] = npr.binomial(1, Pi[zi, zj])
-                X[j, i] = X[i, j]
+	N = Z.shape[0]
+	X = np.zeros((N, N))
+	for i in range(N):
+			zi = find_row(Z[i, :])
+			for j in range(i, N):
+				zj = find_row(Z[j, :])
+				X[i, j] = npr.binomial(1, Pi[zi, zj])
+				X[j, i] = X[i, j]
 
-    return X.astype(float)
+	return X.astype(float)
 
 
 # ############### MODEL CREATION ################
@@ -221,81 +298,87 @@ def sample_X_und(Pi, Z):
 
 def create_msbm(Q, N, M, K):
 
-    print('---- Creating MSBM mode with N = {:d} and K = {:d} ------'
-          .format(N, K))
+	print('---- Creating MSBM mode with N = {:d} and K = {:d} ------'
+		  .format(N, K))
 
-    RHO = get_rho()
+	RHO = get_rho()
 
-    Y = sample_Y(RHO, K)
+	Y = sample_Y(RHO, K)
 
-    GAMMA = np.zeros((M, Q))
-    for m in range(M):
-        GAMMA[m, :] = get_gamma(m)
+	GAMMA = np.zeros((M, Q))
+	for m in range(M):
+		GAMMA[m, :] = get_gamma(m)
 
-    PI = np.zeros((M, Q, Q))
-    for m in range(M):
-        PI[m, :] = get_pi(m)
+	PI = np.zeros((M, Q, Q))
+	for m in range(M):
+		PI[m, :] = get_pi(m)
 
-    Z = np.zeros((K, N, Q))
-    X = np.zeros((K, N, N))
-    for k in range(K):
-        m = find_row(Y[k, :])
-        Z[k, :] = sample_Z(GAMMA[m, :], N)
-        X[k, :] = sample_X_und(PI[m, :], Z[k, :])
+	Z = np.zeros((K, N, Q))
+	X = np.zeros((K, N, N))
+	for k in range(K):
+		m = find_row(Y[k, :])
+		Z[k, :] = sample_Z(GAMMA[m, :], N)
+		X[k, :] = sample_X_und(PI[m, :], Z[k, :])
 
-    par = dict()
-    par['Q'] = Q
-    par['N'] = N
-    par['M'] = M
-    par['K'] = K
+	par = dict()
+	par['Q'] = Q
+	par['N'] = N
+	par['M'] = M
+	par['K'] = K
 
-    data = dict()
-    data['RHO'] = RHO
-    data['Y'] = Y
-    data['GAMMA'] = GAMMA
-    data['PI'] = PI
-    data['Z'] = Z
-    data['X'] = X
+	data = dict()
+	data['RHO'] = RHO
+	data['Y'] = Y
+	data['GAMMA'] = GAMMA
+	data['PI'] = PI
+	data['Z'] = Z
+	data['X'] = X
 
-    return data, par
+	return data, par
 
 
 def find_row(x):
 
-    return np.nonzero(x)[0][0]
+	return np.nonzero(x)[0][0]
 
 # ############### MAIN PROGRAM #####################
 
 
 def main():
+	gamma, pi = get_gamma_pi(
+	vdir = 0.0065, dii = 20.0, dij= 2.0,  
+	Q = 4, sampling = 'constrained', SNR=1,
+	maxIter = 10000, tol = 1e-5, n = 100)
+	print(gamma)
+	print(pi)
+	SystemExit(0)
+	# initialize file names
+	# if len(sys.argv) < 2:
+	#	 path_data = 'data'
+	#	 fname = 'msbm1'
+	#	 data_file_url = path_data + '/' + fname + '.pickle'
 
-    # initialize file names
-    if len(sys.argv) < 2:
-        path_data = 'data'
-        fname = 'msbm1'
-        data_file_url = path_data + '/' + fname + '.pickle'
+	# else:
+	#	 data_file_url = sys.argv[1]
 
-    else:
-        data_file_url = sys.argv[1]
+	# # number of classes
+	# Q = 4
+	# # number of nodes
+	# N = 150
+	# # number of models
+	# M = 4
+	# # number of networks
+	# K = 80
 
-    # number of classes
-    Q = 4
-    # number of nodes
-    N = 150
-    # number of models
-    M = 4
-    # number of networks
-    K = 80
+	# # sample model
+	# data, par = create_msbm(Q, N, M, K)
 
-    # sample model
-    data, par = create_msbm(Q, N, M, K)
-
-    # save to file
-    print('Saving file to {:s} ... '.format(data_file_url), end='')
-    pickle.dump({'data': data, 'par': par}, open(data_file_url, 'wb'))
-    print('saved.')
+	# # save to file
+	# print('Saving file to {:s} ... '.format(data_file_url), end='')
+	# pickle.dump({'data': data, 'par': par}, open(data_file_url, 'wb'))
+	# print('saved.')
 
 
 if __name__ == '__main__':
 
-    main()
+	main()