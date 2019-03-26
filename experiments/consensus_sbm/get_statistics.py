5a1067773c2e371759f217bb74d444fe3dfa3680
Jorge Francisco Barreras <francisco.barreras@quantil.com.co>
2019-03-26 10:32:50 -0400

    Changed to conform to Cassiano Rules

--- experiments/consensus_sbm/get_statistics.py
+++ experiments/consensus_sbm/get_statistics.py
@@ -5,38 +5,40 @@
 import os, sys, inspect
 import pickle
 import numpy as np
-sys.path.insert(0,'../..')
+
+sys.path.insert(0, '../..')
 import util as ut
-import varinf 
+import varinf
+
 
 def main():
+    file_list = os.listdir('data')
+    # Need to read in original data + trained moments and elbos
+    chd_list = np.array(0)
+    ari_list = np.array(0)
+    delta_elbo_list = np.array(0)
+    entropy_list = np.array(0)
+    for data_file in file_list:
+        print("Reading data + moments from model: {}".format(data_file))
+        # load data
+        data, par = ut.load_data('data/' + data_file)
+        results_mom, elbo_seq = ut.load_results('data/' + data_file)
 
-	file_list = os.listdir('data')
-	#Need to read in original data + trained moments and elbos
-	chd_list = np.array(0)
-	ari_list = np.array(0)
-	delta_elbo_list = np.array(0)
-	entropy_list = np.array(0)
-	for data_file in file_list:
-		print("Reading data + moments from model: {}".format(data_file))
-		#load data
-		data, par = ut.load_data('data/' + data_file)
-		results_mom, elbo_seq = ut.load_results('data/' + data_file)
+        chd_list = np.append(chd_list, par['SNR'])
+        # Adjusted rand index
+        mari_Z = np.mean(adj_rand_Z(results_mom, data))
+        ari_list = np.append(ari_list, mari_Z)
+        # Percentage elbo increase
+        delta_elbo = (elbos['all'][-1] - elbos['all'][0]) / np.abs(elbos['all'][0])
+        delta_elbo_list = np.append(delta_elbo_list, delta_elbo)
+        # Mean z entropy
+        entro = np.mean(get_entropy_Z(results_mom))
+        entropy_list = np.append(entropy_list, entro)
 
-		chd_list = np.append(chd_list,par['SNR'])
-		#Adjusted rand index
-		mari_Z = np.mean(adj_rand_Z(results_mom, data))
-		ari_list = np.append(ari_list, mari_Z)
-		#Percentage elbo increase
-		delta_elbo = (elbos['all'][-1] - elbos['all'][0])/np.abs(elbos['all'][0])
-		delta_elbo_list = np.append(delta_elbo_list, delta_elbo)
-		#Mean z entropy
-		entro = np.mean(get_entropy_Z(results_mom))
-		entropy_list = np.append(entropy_list,entro)
+        print('Saving file to {:s} ... '.format('models/model_' + data_file))
+        pickle.dump({'results_mom': results_mom, 'elbo_seq': elbo_seq}, open('models/model_' + data_file, 'wb'))
+    sys.exit()
 
-		print('Saving file to {:s} ... '.format('models/model_' + data_file))
-		pickle.dump({'results_mom': results_mom, 'elbo_seq': elbo_seq}, open('models/model_' + data_file, 'wb'))
-	sys.exit()
 
 if __name__ == '__main__':
     main()
