5f76f80296c1a353622f45e795846214e627aff5
Jorge Francisco Barreras <francisco.barreras@quantil.com.co>
2019-03-26 10:24:43 -0400

    Completed get_statistics.py

--- experiments/consensus_sbm/get_statistics.py
+++ experiments/consensus_sbm/get_statistics.py
@@ -13,6 +13,7 @@ def main():
 
 	file_list = os.listdir('data')
 	#Need to read in original data + trained moments and elbos
+	chd_list = np.array(0)
 	ari_list = np.array(0)
 	delta_elbo_list = np.array(0)
 	entropy_list = np.array(0)
@@ -22,6 +23,7 @@ def main():
 		data, par = ut.load_data('data/' + data_file)
 		results_mom, elbo_seq = ut.load_results('data/' + data_file)
 
+		chd_list = np.append(chd_list,par['SNR'])
 		#Adjusted rand index
 		mari_Z = np.mean(adj_rand_Z(results_mom, data))
 		ari_list = np.append(ari_list, mari_Z)
