18629e18a9e5a81e24267ec91c9244b4db9fb8c1
Jorge Francisco Barreras <francisco.barreras@quantil.com.co>
2019-03-26 11:04:41 -0400

    added chd_list to the statistics

--- experiments/consensus_sbm/get_statistics.py
+++ experiments/consensus_sbm/get_statistics.py
@@ -14,6 +14,7 @@ import varinf
 def main():
     file_list = os.listdir('data')
     # Need to read in original data + trained moments and elbos
+    chd_list = np.array(0)
     ari_list = np.array(0)
     delta_elbo_list = np.array(0)
     entropy_list = np.array(0)
@@ -24,6 +25,7 @@ def main():
         model_url = os.path.join('models', 'model_' + data_file)
         results_mom, elbo_seq = ut.load_results(model_url)
 
+        chd_list = np.append(chd_list, par['SNR'])
         # Adjusted rand index
         mari_Z = np.mean(ut.adj_rand_Z(results_mom, data))
         ari_list = np.append(ari_list, mari_Z)
@@ -36,7 +38,7 @@ def main():
 
     stats_url = os.path.join('stats', 'stats_' + 'consensus')
     print('Saving file to {:s} ... '.format(stats_url))
-    stats_dict = {'ari_list': ari_list, 'delta_elbo_list': delta_elbo_list, 'entropy_list': entropy_list}
+    stats_dict = {'chd_list': chd_list, 'ari_list': ari_list, 'delta_elbo_list': delta_elbo_list, 'entropy_list': entropy_list}
     pickle.dump(stats_dict, open(stats_url, 'wb'))
     sys.exit()
 
